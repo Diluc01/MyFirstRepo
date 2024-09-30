9bce658ff84f5d6040538ba5701aa718083bac1e
Berlin Tejeda <berlintejeda22@gmail.com>
2024-09-30 18:53:51 -0400

    Solved second problem

--- JS_Intro/Loops/ALAB308_3_1.js
+++ JS_Intro/Loops/ALAB308_3_1.js
@@ -1,3 +1,4 @@
+// First problem
 for (let i = 1; i < 100; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
     console.log("Fizz Buzz");
@@ -5,5 +6,25 @@ for (let i = 1; i < 100; i++) {
     console.log("Fizz");
   } else if (i % 5 === 0) {
     console.log("Buzz");
+  } else {
+    console.log(i);
   }
 }
+
+// Second problem
+let n = 8;
+
+for (let i = n; i <= 12; i++) {
+  let prime = i > 1;
+  for (let j = 2; j <= Math.sqrt(i); j++) {
+    if (i % j === 0) {
+      prime = false;
+      break;
+    }
+  }
+  if (prime) {
+    console.log(i, "is a prime number");
+  }
+}
+
+// third problem
